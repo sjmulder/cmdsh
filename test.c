e51b317a46eda8554d6471e88787dda9d62e4209
Sijmen J. Mulder <ik@sjmulder.nl>
2024-07-20 14:13:16 +0200

    Descriptive test names

--- test.c
+++ test.c
@@ -10,110 +10,109 @@ test_to_argv(void)
 {
 	static char buf[512];
 	static char *argv[16];
-	const char *input;
 	int argc;
 
-	printf("to_argv: %s\n", input = "");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: empty string");
+	argc = to_argv("", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 0);
 	assert(!argv[0]);
 
-	printf("to_argv: %s\n", input = "ls");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: single word");
+	argc = to_argv("ls", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 1);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!argv[1]);
 
-	printf("to_argv: %s\n", input = "ls code");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: two words");
+	argc = to_argv("ls code", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 2);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "code"));
 	assert(!argv[2]);
 
-	printf("to_argv: %s\n", input = "\tls   code ");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: whitespace");
+	argc = to_argv("\tls  code ", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 2);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "code"));
 	assert(!argv[2]);
 
-	printf("to_argv: %s\n", input = "ls code\\foo");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: backslash translation");
+	argc = to_argv("ls code\\foo", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 2);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "code/foo"));
 	assert(!argv[2]);
 
-	printf("to_argv: %s\n", input = "ls /l");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: slash to hyphen translation");
+	argc = to_argv("ls /l", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 2);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "-l"));
 	assert(!argv[2]);
 
-	printf("to_argv: %s\n", input = "ls /l /a");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: slash to hyphen translation, multiple");
+	argc = to_argv("ls /l /a", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 3);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "-l"));
 	assert(!strcmp(argv[2], "-a"));
 	assert(!argv[3]);
 
-	printf("to_argv: %s\n", input = "ls/l/a");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: multiple slash-arguments without space");
+	argc = to_argv("ls/l/a", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 3);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "-l"));
 	assert(!strcmp(argv[2], "-a"));
 	assert(!argv[3]);
 
-	printf("to_argv: %s\n", input = "ls /l /all");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: longopt slash to double hyphen translation");
+	argc = to_argv("ls /l /all", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 3);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "-l"));
 	assert(!strcmp(argv[2], "--all"));
 	assert(!argv[3]);
 
-	printf("to_argv: %s\n", input = "ls/l/all");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: longopt slash arguments without space");
+	argc = to_argv("ls/l/all", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 3);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "-l"));
 	assert(!strcmp(argv[2], "--all"));
 	assert(!argv[3]);
 
-	printf("to_argv: %s\n", input = "ls \"\"");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: empty quotes");
+	argc = to_argv("ls \"\"", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 2);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], ""));
 	assert(!argv[2]);
 
-	printf("to_argv: %s\n", input = "ls \"code/foo\"");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: quoted path");
+	argc = to_argv("ls \"code/foo\"", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 2);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "code/foo"));
 	assert(!argv[2]);
 
-	printf("to_argv: %s\n", input = "ls code\"/\"foo");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: embeded quoted part");
+	argc = to_argv("ls code\"/\"foo", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 2);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "code/foo"));
 	assert(!argv[2]);
 
-	printf("to_argv: %s\n", input = "ls \"code  foo\"");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: quoted spaces");
+	argc = to_argv("ls \"code  foo\"", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 2);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "code  foo"));
 	assert(!argv[2]);
 
-	printf("to_argv: %s\n", input = "ls -la");
-	argc = to_argv(input, buf, LEN(buf), argv, LEN(argv));
+	puts("to_argv: quoted flags");
+	argc = to_argv("ls -la", buf, LEN(buf), argv, LEN(argv));
 	assert(argc == 2);
 	assert(!strcmp(argv[0], "ls"));
 	assert(!strcmp(argv[1], "-la"));
